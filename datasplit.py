36e8e6831e4e68c68269b329f3abde27d12478e5
doris <calamintha101@gmail.com>
2021-03-23 20:41:49 +0100

    couldn't make it work to have train, valid and test sets. Now copy the holy test set away in other directory before starting. then use keras to divide test and valid set.

--- datasplit.py
+++ datasplit.py
@@ -5,9 +5,59 @@ from sklearn.model_selection import train_test_split
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img
 import matplotlib.pyplot as plt
+import os, shutil
+import random
 
 ##
-full_dataset = 'C:/git_zhaw/catnocat/data/raw/'
+data_path = 'C:/git_zhaw/catnocat/data'
+orig_dataset = os.path.join(data_path, 'raw')
+
+trainvalid_path = os.path.join(data_path, 'trainvalid')
+holytest_path = os.path.join(data_path, 'holytest')
+
+##
+
+#
+if os.path.exists(trainvalid_path):
+    shutil.rmtree(trainvalid_path)
+
+# copy the whole dataset into a new directory
+print('Copying the original images to a new directory. This takes a while....')
+shutil.copytree(orig_dataset, trainvalid_path)
+
+##
+# move 20% of the data out of the testvalid set into the holy test set
+holy_testset_proportion = 0.2
+
+cat_files = os.listdir(os.path.join(orig_dataset ,'cat'))
+nocat_files = os.listdir(os.path.join(orig_dataset ,'nocat'))
+
+random.seed(2384)
+holy_test_filenames = {'cat':   random.sample(cat_files, int(len(cat_files) * holy_testset_proportion)),
+                       'nocat': random.sample(nocat_files, int(len(nocat_files) * holy_testset_proportion))
+                       }
+
+print("Holy Test files for Cats: ", len(holy_test_filenames['cat']))
+print("Holy Test files for NoCats: ", len(holy_test_filenames['nocat']))
+
+# move the holy test files to a dedicated directory
+if os.path.exists(holytest_path):
+    shutil.rmtree(holytest_path)
+
+##
+os.mkdir(holytest_path)
+
+# move the cat test files:
+
+for c in holy_test_filenames:
+    print('Moving files for ', c)
+    subFolder = os.path.join(holytest_path, c)
+    os.makedirs(subFolder)
+
+    for file in holy_test_filenames[c]:
+        shutil.move(os.path.join(trainvalid_path, c, file), subFolder)
+
+
 
 ##
 
@@ -25,28 +75,34 @@ batch_size = 32
 
 ##
 image_generator = ImageDataGenerator(rescale=1/255,
-                                     validation_split=0.4)
+                                     validation_split=0.2)
 
 train_dataset = image_generator.flow_from_directory(batch_size=batch_size,
-                                                    directory=full_dataset,
+                                                    directory=trainvalid_path,
                                                     shuffle=True,
                                                     target_size=image_size,
                                                     subset="training",
                                                     class_mode='categorical')
 
-validation_dataset = image_generator.flow_from_directory(batch_size=batch_size,
-                                                         directory=full_dataset,
+valid_dataset = image_generator.flow_from_directory(batch_size=batch_size,
+                                                         directory=trainvalid_path,
                                                          shuffle=True,
                                                          target_size=image_size,
                                                          subset="validation",
                                                          class_mode='categorical')
 
 ##
-
+print("TrainingSet Total")
 print("Number of samples: ", train_dataset.samples)
 print("Number of classes: ", len(train_dataset.class_indices))
 print("Classes ", train_dataset.class_indices)
-print("files: ", train_dataset.filenames[0:5])
+#print("files: ", train_dataset.filenames[0:5])
+
+print("\nValidation Set Total")
+print("Number of samples: ", valid_dataset.samples)
+print("Number of classes: ", len(valid_dataset.class_indices))
+print("Classes ", valid_dataset.class_indices)
+
 
 ##
 x, y = next(train_dataset)
@@ -65,4 +121,6 @@ for i in range(9):
     plt.axis("off")
 
 plt.show()
+
+
 
