2261a66283fb195382fa6aeecd41c90612143d8b
doris <calamintha101@gmail.com>
2021-03-27 12:08:57 +0100

    Don't use the validation split function from the data generator. Split data manually in different directories. Display all images of one batch of 32.

--- datasplit.py
+++ datasplit.py
@@ -1,63 +1,103 @@
 ##
 # import matplotlib.pyplot as plt
-import numpy as np
-from sklearn.model_selection import train_test_split
-import tensorflow as tf
-from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img
-import matplotlib.pyplot as plt
-import os, shutil
+import os
 import random
+import shutil
+
+import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 ##
 data_path = 'C:/git_zhaw/catnocat/data'
 orig_dataset = os.path.join(data_path, 'raw')
+cat_orig_files = os.listdir(os.path.join(orig_dataset, 'cat'))
+nocat_orig_files = os.listdir(os.path.join(orig_dataset, 'nocat'))
 
-trainvalid_path = os.path.join(data_path, 'trainvalid')
-holytest_path = os.path.join(data_path, 'holytest')
+train_data_path = os.path.join(data_path, 'train')
+valid_data_path = os.path.join(data_path, 'valid')
+holytest_data_path = os.path.join(data_path, 'holytest')
 
 ##
-
-#
-if os.path.exists(trainvalid_path):
-    shutil.rmtree(trainvalid_path)
+if os.path.exists(train_data_path):
+    shutil.rmtree(train_data_path)
 
 # copy the whole dataset into a new directory
 print('Copying the original images to a new directory. This takes a while....')
-shutil.copytree(orig_dataset, trainvalid_path)
+shutil.copytree(orig_dataset, train_data_path)
 
 ##
-# move 20% of the data out of the testvalid set into the holy test set
-holy_testset_proportion = 0.2
+cat_files = os.listdir(os.path.join(orig_dataset, 'cat'))
+nocat_files = os.listdir(os.path.join(orig_dataset, 'nocat'))
 
-cat_files = os.listdir(os.path.join(orig_dataset ,'cat'))
-nocat_files = os.listdir(os.path.join(orig_dataset ,'nocat'))
+print("Number of cat files: ", len(cat_files))
+print("Number of nocat files: ", len(nocat_files))
 
+##
+if os.path.exists(holytest_data_path):
+    shutil.rmtree(holytest_data_path)
+os.mkdir(holytest_data_path)
+
+if os.path.exists(valid_data_path):
+    shutil.rmtree(valid_data_path)
+os.mkdir(valid_data_path)
+
+
+##
+# move 20% of the data out of the training set into the validation set
+proportion = 0.2
+num_cat_files = int(len(cat_orig_files) * proportion)
+num_nocat_files = int(len(nocat_orig_files) * proportion)
+
+cat_files = os.listdir(os.path.join(train_data_path, 'cat'))
+nocat_files = os.listdir(os.path.join(train_data_path, 'nocat'))
+
+##
 random.seed(2384)
-holy_test_filenames = {'cat':   random.sample(cat_files, int(len(cat_files) * holy_testset_proportion)),
-                       'nocat': random.sample(nocat_files, int(len(nocat_files) * holy_testset_proportion))
-                       }
+validset_filenames = {'cat': random.sample(cat_files, num_cat_files),
+                      'nocat': random.sample(nocat_files, num_nocat_files)
+                      }
 
-print("Holy Test files for Cats: ", len(holy_test_filenames['cat']))
-print("Holy Test files for NoCats: ", len(holy_test_filenames['nocat']))
+print("Validation files for Cats: ", len(validset_filenames['cat']))
+print("Validation files for NoCats: ", len(validset_filenames['nocat']))
 
-# move the holy test files to a dedicated directory
-if os.path.exists(holytest_path):
-    shutil.rmtree(holytest_path)
-
-##
-os.mkdir(holytest_path)
-
-# move the cat test files:
-
-for c in holy_test_filenames:
+# move the validation files to a dedicated directory
+for c in validset_filenames:
     print('Moving files for ', c)
-    subFolder = os.path.join(holytest_path, c)
+    subFolder = os.path.join(valid_data_path, c)
     os.makedirs(subFolder)
 
-    for file in holy_test_filenames[c]:
-        shutil.move(os.path.join(trainvalid_path, c, file), subFolder)
+    for file in validset_filenames[c]:
+        shutil.move(os.path.join(train_data_path, c, file), subFolder)
+
+print("Number of cat files left:   ", len(os.listdir(os.path.join(train_data_path, 'cat'))))
+print("Number of nocat fiels left: ", len(os.listdir(os.path.join(train_data_path, 'nocat'))))
+
+##
+# move 20% of the data out of the train set into the holy test set
+cat_files = os.listdir(os.path.join(train_data_path, 'cat'))
+nocat_files = os.listdir(os.path.join(train_data_path, 'nocat'))
+
+random.seed(2384)
+random.seed(2384)
+holytest_filenames = {'cat': random.sample(cat_files, num_cat_files),
+                      'nocat': random.sample(nocat_files, num_nocat_files)
+                      }
+
+print("Holy Test files for Cats: ", len(holytest_filenames['cat']))
+print("Holy Test files for NoCats: ", len(holytest_filenames['nocat']))
+
+# move the holy test files to a dedicated directory
+for c in holytest_filenames:
+    print('Moving files for ', c)
+    subFolder = os.path.join(holytest_data_path, c)
+    os.makedirs(subFolder)
+
+    for file in holytest_filenames[c]:
+        shutil.move(os.path.join(train_data_path, c, file), subFolder)
 
 
+print("Number of cat files left:   ", len(os.listdir(os.path.join(train_data_path, 'cat'))))
+print("Number of nocat fiels left: ", len(os.listdir(os.path.join(train_data_path, 'nocat'))))
 
 ##
 
@@ -74,53 +114,63 @@ image_size = (280, 280)
 batch_size = 32
 
 ##
-image_generator = ImageDataGenerator(rescale=1/255,
-                                     validation_split=0.2)
+# Set Data Generator for training, testing and validataion.
 
-train_dataset = image_generator.flow_from_directory(batch_size=batch_size,
-                                                    directory=trainvalid_path,
-                                                    shuffle=True,
-                                                    target_size=image_size,
-                                                    subset="training",
-                                                    class_mode='categorical')
+# Note for testing, set shuffle = false (For proper Confusion matrix)
 
-valid_dataset = image_generator.flow_from_directory(batch_size=batch_size,
-                                                         directory=trainvalid_path,
-                                                         shuffle=True,
-                                                         target_size=image_size,
-                                                         subset="validation",
-                                                         class_mode='categorical')
+
+train_datagen = ImageDataGenerator(rescale=1/255)
+train_dataset = train_datagen.flow_from_directory(train_data_path,
+                                                  target_size=image_size,
+                                                  batch_size=batch_size,
+                                                  class_mode='categorical',
+                                                  shuffle=True)
+
+valid_datagen = ImageDataGenerator(rescale=1/255)
+valid_dataset = valid_datagen.flow_from_directory(valid_data_path,
+                                                  target_size=image_size,
+                                                  batch_size=batch_size,
+                                                  class_mode='categorical',
+                                                  shuffle=True)
+
+test_datagen = ImageDataGenerator(rescale=1/255)
+test_dataset = test_datagen.flow_from_directory(holytest_data_path,
+                                                target_size=image_size,
+                                                batch_size=batch_size,
+                                                class_mode='categorical',
+                                                shuffle=False)
 
 ##
 print("TrainingSet Total")
 print("Number of samples: ", train_dataset.samples)
 print("Number of classes: ", len(train_dataset.class_indices))
 print("Classes ", train_dataset.class_indices)
-#print("files: ", train_dataset.filenames[0:5])
+# print("files: ", train_dataset.filenames[0:5])
 
 print("\nValidation Set Total")
 print("Number of samples: ", valid_dataset.samples)
 print("Number of classes: ", len(valid_dataset.class_indices))
 print("Classes ", valid_dataset.class_indices)
 
+print("\nTest Set Total")
+print("Number of samples: ", test_dataset.samples)
+print("Number of classes: ", len(test_dataset.class_indices))
+print("Classes ", test_dataset.class_indices)
 
 ##
 x, y = next(train_dataset)
-print("x: ", type(x))
-print("y: ", type(y))
+print("Batch Index: ", train_dataset.batch_index)
 print("x: ", x.shape)
 print("y: ", y.shape)
 
-
-##
-plt.figure(figsize=(10, 10))
-for i in range(9):
-    ax = plt.subplot(3, 3, i + 1)
+plt.figure(figsize=(20, 10))
+for i in range(32):
+    ax = plt.subplot(4, 8, i + 1)
     plt.imshow(x[i])
     plt.title(y[i])
     plt.axis("off")
 
 plt.show()
 
-
+##
 
